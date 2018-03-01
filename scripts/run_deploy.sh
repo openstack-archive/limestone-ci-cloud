dd6563c109fc5fdbb5c8329c8aab414633f15d61
Logan V <logan2211@gmail.com>
2018-03-01 18:10:51 +0000

    Add a basic deploy script

---
+++ scripts/run_deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+export SCRIPT_PATH=$(dirname $(readlink -f "$0"))
+export PROJECT_PATH=$(dirname "$SCRIPT_PATH")
+
+cp -R ${PROJECT_PATH}/openstack_deploy /etc
+pushd ${PROJECT_PATH}/openstack-ansible
+scripts/bootstrap-ansible.sh
+popd
+
+pushd ${PROJECT_PATH}/openstack-ansible/playbooks
+openstack-ansible setup-hosts.yml
+openstack-ansible setup-infrastructure.yml
+openstack-ansible setup-openstack.yml
+popd
