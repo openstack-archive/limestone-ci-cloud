0f9ba783142f7e609f75f69e898535eed7f65962
Logan V <logan2211@gmail.com>
2019-03-17 13:45:54 -0500

    Re-add ELK deployment including bootstrapper
    Change-Id: Iedac481e593c1f0df8ae9d5d5af20fb280992e94

--- scripts/run_deploy.sh
+++ scripts/run_deploy.sh
@@ -20,3 +20,16 @@ popd
 pushd ${PROJECT_PATH}/network_bootstrap
 openstack-ansible bootstrap-neutron.yml
 popd
+
+pushd ${PROJECT_PATH}/elk_bootstrap
+openstack-ansible bootstrap-elk-infra.yml
+popd
+
+pushd ${PROJECT_PATH}/openstack-ansible-ops/elk_metrics_6x
+source bootstrap-embedded-ansible.sh || true
+# Workaround until https://review.openstack.org/#/c/643758/ is merged
+export ANSIBLE_STRATEGY_PLUGINS="${HOME}/ansible_venv/repositories/roles/plugins/strategy"
+export ANSIBLE_INVENTORY="${ANSIBLE_INVENTORY},${PROJECT_PATH}/elk_bootstrap/inventory"
+ansible-playbook "${PROJECT_PATH}/elk_bootstrap/install.yml" ${USER_VARS} -e 'elk_package_state="latest"'
+deactivate
+popd
